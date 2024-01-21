d1cef1367f79a6bfd4a21480b76bc972b8603d0d
Watch <dwatchr.man@gmail.com>
2024-01-21 21:02:25 +0600

    customized radix ui theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,15 @@
-"use client"
+"use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
 
 const NewIssuePage = () => {
     return (
-        <div className="max-w-xl space-y-3">
+        <div className='max-w-xl space-y-3'>
+            <h1 className='text-2xl font-semi-bold'>Create a New issue</h1>
             <TextField.Root>
                 <TextField.Input placeholder='Title' />
             </TextField.Root>
-            <TextArea placeholder="Description..." />
+            <TextArea placeholder='Description...' />
             <Button>Submit New Issue</Button>
         </div>
     );
