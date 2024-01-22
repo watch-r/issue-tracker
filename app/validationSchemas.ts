a0b2123850ed262b882de9c38e08cb7fa9f85d67
Watch <dwatchr.man@gmail.com>
2024-01-23 00:35:28 +0600

    build api for updating issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const createIssueSchema = z.object({
+export const issueSchema = z.object({
     title: z.string().min(1, "A Title is Required").max(255),
     description: z.string().min(1, "A small Description is Required"),
 });
