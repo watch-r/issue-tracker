bceaa8a28d98bd5be2647d5003aba4b49e8d0ea7
Watch <dwatchr.man@gmail.com>
2024-01-23 13:48:14 +0600

    improve user experience

--- app/issues/[id]/DeleteissueButton.tsx
+++ app/issues/[id]/DeleteissueButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Spinner } from "@/app/components";
 import { TrashIcon } from "@radix-ui/react-icons";
 import { AlertDialog, Button, Flex, Text } from "@radix-ui/themes";
 import axios from "axios";
@@ -8,12 +9,16 @@ import { useState } from "react";
 const DeleteissueButton = ({ issueId }: { issueId: number }) => {
     const router = useRouter();
     const [error, setError] = useState(false);
+    const [isDeleting, setDeleting] = useState(false);
+
     const deleteIssue = async () => {
         try {
+            setDeleting(true);
             await axios.delete("/api/issues/" + issueId);
             router.push("/issues");
             router.refresh();
         } catch (error) {
+            setDeleting(false);
             setError(true);
         }
     };
@@ -21,9 +26,14 @@ const DeleteissueButton = ({ issueId }: { issueId: number }) => {
         <>
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
-                    <Button color='crimson' variant='surface'>
+                    <Button
+                        color='crimson'
+                        variant='surface'
+                        disabled={isDeleting}
+                    >
                         <TrashIcon />
-                        <Text className='text-xs'>Delete Issue</Text>
+                        <Text>Delete</Text>
+                        {isDeleting && <Spinner />}
                     </Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Content style={{ maxWidth: 450 }}>
