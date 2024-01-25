8711e8bc9f926ea3592175f5a5c811cd18cf8184
Watch <dwatchr.man@gmail.com>
2024-01-25 13:50:20 +0600

    Built Latest issue Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import LatestIssue from "./LatestIssue";
 import Pagination from "./components/Pagination";
 
-export default function Home({
-    searchParams,
-}: {
-    searchParams: { page: string };
-}) {
-    return <Pagination currentpage={parseInt(searchParams.page)} itemCount={100} pageSize={10} />;
+export default function Home() {
+    return (
+        <>
+            <LatestIssue/>
+        </>
+    );
 }
