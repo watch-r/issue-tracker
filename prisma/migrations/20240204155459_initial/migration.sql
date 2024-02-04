b1ac39846514f6da0820d93757a903f0ce3de4cc
Watch <dwatchr.man@gmail.com>
2024-02-04 22:58:56 +0600

    ... ...

--- prisma/migrations/20240204155459_initial/migration.sql
+++ prisma/migrations/20240204155459_initial/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Issue` (
     `title` VARCHAR(255) NOT NULL,
     `description` TEXT NOT NULL,
     `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME NOT NULL,
     `assignedToUserId` VARCHAR(255) NULL,
 
