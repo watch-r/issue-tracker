231520188f997c9b6ef690cff32743f5e7aa4ad9
Watch <dwatchr.man@gmail.com>
2024-02-04 23:00:32 +0600

    ... ... ...

--- prisma/migrations/20240204155459_initial/migration.sql
+++ prisma/migrations/20240204155459_initial/migration.sql
@@ -46,7 +46,7 @@ CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `email` VARCHAR(191) NULL,
-    `emailVerified` DATETIME(3) NULL,
+    `emailVerified` DATETIME NULL,
     `image` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
